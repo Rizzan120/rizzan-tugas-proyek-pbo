3bcdc4af15129cc15d53f8dbae3cabef30ece338
Rizzan120 <2300018350@webmail.uad.ac.id>
2024-12-13 15:14:06 +0700

    Delete Bus.java

--- Bus.java
+++
@@ -1,5 +0,0 @@
-public class Bus extends Kendaraan {
-    public Bus(String idKendaraan, int kapasitas, boolean statusOperasional, Rute rute) {
-        super(idKendaraan, "Bus", kapasitas, statusOperasional, rute);
-    }
-}
