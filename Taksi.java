390de112d372a961b7ea3da765ea6f4fb16d1363
Rizzan120 <2300018350@webmail.uad.ac.id>
2024-12-13 15:16:21 +0700

    Delete Taksi.java

--- Taksi.java
+++
@@ -1,5 +0,0 @@
-public class Taksi extends Kendaraan {
-    public Taksi(String idKendaraan, int kapasitas, boolean statusOperasional, Rute rute) {
-        super(idKendaraan, "Taksi", kapasitas, statusOperasional, rute);
-    }
-}
