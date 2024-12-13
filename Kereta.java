bb7dccf0a85c56d75a1563370dba28d84c44ae48
Rizzan120 <2300018350@webmail.uad.ac.id>
2024-12-13 15:15:10 +0700

    Delete Kereta.java

--- Kereta.java
+++
@@ -1,5 +0,0 @@
-public class Kereta extends Kendaraan {
-    public Kereta(String idKendaraan, int kapasitas, boolean statusOperasional, Rute rute) {
-        super(idKendaraan, "Kereta", kapasitas, statusOperasional, rute);
-    }
-}
