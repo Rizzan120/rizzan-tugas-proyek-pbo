0f17ecb5b90756096afdb31bc056611c1e0d00b8
Rizzan120 <2300018350@webmail.uad.ac.id>
2024-12-13 14:55:51 +0700

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,5 @@
+public class Bus extends Kendaraan {
+    public Bus(String idKendaraan, int kapasitas, boolean statusOperasional, Rute rute) {
+        super(idKendaraan, "Bus", kapasitas, statusOperasional, rute);
+    }
+}
---
+++ Taksi.java
@@ -0,0 +1,5 @@
+public class Taksi extends Kendaraan {
+    public Taksi(String idKendaraan, int kapasitas, boolean statusOperasional, Rute rute) {
+        super(idKendaraan, "Taksi", kapasitas, statusOperasional, rute);
+    }
+}
